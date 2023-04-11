96f51a8e23700cd02e985b729184c2c5eadad93d
gladysgodwin <gladysgodwin79@gmail.com>
2023-04-11 14:48:57 +0100

    using s3 as remote state

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,11 +1,10 @@
 terraform {
-  cloud {
-    organization = "AltSchoolProject-byGladys"
-
-    workspaces {
-      name = "jenkins-project"
-    }
+  backend "s3" {
+    bucket = "my-terraform-state-bucket"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
   }
+
 
   required_providers {
     aws = {
